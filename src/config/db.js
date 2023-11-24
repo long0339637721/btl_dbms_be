35598390e9d7fdfa961f08724c0121302b7273fe
Long <long01639637721@gmail.com>
2023-11-25 02:34:42 +0700

    connect to db

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,23 @@
-const mysql = require('mysql2')
+const mssql = require('mssql')
 require('dotenv').config();
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+const connection = mssql.connect({
+  server: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  options: {
+    trustServerCertificate: true
+  }
 })
-
-connection.connect(err => {
-    if (err) console.error('EEError connecting to database: ', err)
-    console.log('Connected!')
-})
+  .then(pool => {
+    return pool.request().query('SELECT * FROM categories');
+  })
+  .then(result => {
+    console.log('Connected: ', result.recordset);
+  })
+  .catch(error => {
+    console.error('Connect err: ', error);
+  });
 
 module.exports = connection
